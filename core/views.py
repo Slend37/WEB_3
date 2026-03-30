379a05e232c144cb1f930a54255d317498b72bb0
Alexander Malandin <SlendermanGame18@mail.ru>
2026-03-30 11:24:37 +0300

    Task 4

--- core/views.py
+++ core/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'core/index.html')
+    context = {
+        'title': 'Сервис обмена книгами',
+        'welcome_text': 'Удобный и простой сайт для обмена книгами онлайн.'
+    }
+    return render(request, 'core/index.html', context)
+
+def about(request):
+    return render(request, 'core/about.html')

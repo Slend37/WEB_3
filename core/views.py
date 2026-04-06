b2cc584134b3257f379941c7e202920f6bf47ed1
Alexander Malandin <SlendermanGame18@mail.ru>
2026-04-06 12:56:32 +0300

    Task 6 ready

--- core/views.py
+++ core/views.py
@@ -1,9 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from core.models import Book
+from django.http import HttpRequest, HttpResponse
+
 
 def index(request):
     context = {
         'title': 'Сервис обмена книгами',
-        'welcome_text': 'Удобный и простой сайт для обмена книгами онлайн.'
+        'welcome_text': 'Удобный и простой сайт для обмена книгами онлайн.',
+        'books': Book.objects.order_by("-name").all(),
     }
     return render(request, 'core/index.html', context)
 
